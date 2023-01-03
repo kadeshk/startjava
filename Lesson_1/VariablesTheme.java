729e71025187cb0194860d2ee22c37adc506346c
kadeshk <life1508555@gmail.com>
2023-01-03 14:57:20 +0300

    Исправил ошибки в VariablesTheme.java

--- Lesson_1/VariablesTheme.java
+++ Lesson_1/VariablesTheme.java
@@ -4,77 +4,66 @@ public class VariablesTheme {
         System.out.println("Задачки на тему \"Переменные: примитивные типы данных\"");
         System.out.println("Задание выполнил Александр sb2024@yandex.ru \n");
         System.out.println("1.Вывод значений переменных на консоль");
-        byte coreNumber = 8;
-        float hzNum = 1.2f;
-        short ramMemoryNum = 16;
+        byte coreNum = 8;
+        float ghzNum = 1.2f;
+        short ramNum = 16;
         String graphicsCoprocessorName = "NVIDIA GeForce RTX 3060";
         int graphicsCoprocessorNum = 2048 ;
         short ssdSize = 512;
         long hddSize = 1;
         char series = 'W';
         double weight = 10.23;
-        boolean isTrue = true;
+        boolean licenseStatus = true;
 
-        System.out.println("Количество ядер: " + coreNumber);
-        System.out.println("Частота процессора:  " + hzNum + " Гц");
-        System.out.println("Объем оперативной памяти: " + ramMemoryNum + " ГБ");
+        System.out.println("Количество ядер: " + coreNum);
+        System.out.println("Частота процессора:  " + ghzNum + " Гц");
+        System.out.println("Объем оперативной памяти: " + ramNum + " ГБ");
         System.out.println("Объем памяти видеокарты: " + graphicsCoprocessorNum + " МБ");
         System.out.println("Общий объем накопителей SSD: " + ssdSize + " ГБ");
         System.out.println("Общий объем накопителей HDD: " + hddSize + " ТБ");
         System.out.println("Серия: " + series);
         System.out.println("Вес: " + weight + " кг");
-        System.out.println("Наличие лицензионной версии: " + isTrue);
+        System.out.println("Наличие лицензионной версии: " + licenseStatus);
 
         System.out.println("\n 2.Расчет стоимости товара со скидкой");
-        int pen = 100;
-        int book = 200;
+        int penPrice = 100;
+        int bookPrice = 200;
         float discount = 0.11f;
-        float sumOfTheDiscount = (pen + book) * discount;
-        float sumPriceOfAll = ((pen + book) - sumOfTheDiscount);
+        float discountSum = (penPrice + bookPrice) * discount;
+        float discountPrice = ((penPrice + bookPrice) - discountSum);
 
-        System.out.println("I. Сумма скидки: " + sumOfTheDiscount + " руб");
-        System.out.println("II. Общая стоимость товаров со скидкой: " + sumPriceOfAll + " руб");
+        System.out.println("I. Сумма скидки: " + discountSum + " руб");
+        System.out.println("II. Общая стоимость товаров со скидкой: " + discountPrice + " руб");
 
         System.out.println("\n 3.Вывод слова JAVA");
-        System.out.println("   J    a  v     v  a      ");
-        System.out.println("   J   a a  v   v  a a     ");
-        System.out.println("J  J  aaaaa  V V  aaaaa    ");
-        System.out.println(" JJ  a     a  V  a     a   ");
+        System.out.println("   J    a  v     v  a");
+        System.out.println("   J   a a  v   v  a a");
+        System.out.println("J  J  aaaaa  V V  aaaaa");
+        System.out.println(" JJ  a     a  V  a     a");
 
         System.out.println("\n 4.Вывод min и max значений целых числовых типов");
-        byte byteNum = 127;
-        short shortNum = 32767;
-        int intNum = 2147483647;
-        long longNum = 922337203685477580l;
-        System.out.println("Первоначальное значение");
-        System.out.println(byteNum);
-        System.out.println(shortNum);
-        System.out.println(intNum);
-        System.out.println(longNum);
+        byte byteMax = 127;
+        short shortMax = 32767;
+        int intMax = 2147483647;
+        long longMax = 922337203685477580l;
+        System.out.println("Первоначальное значение \n" + byteMax + "\n" + shortMax + "\n" + intMax + "\n" + longMax);
 
-        System.out.println("Значение после инкремента");
-        byteNum++;
-        shortNum++;
-        intNum++;
-        longNum++;
-        System.out.println(byteNum);
-        System.out.println(shortNum);
-        System.out.println(intNum);
-        System.out.println(longNum);
+        byteMax++;
+        shortMax++;
+        intMax++;
+        longMax++;
+        System.out.println("\n Значение после инкремента \n" + byteMax + "\n" + shortMax + "\n" + intMax + "\n" + longMax);
 
-        System.out.println("Значение после декремента");
-        byteNum--;
-        shortNum--;
-        intNum--;
-        longNum--;
-        System.out.println(byteNum);
-        System.out.println(shortNum);
-        System.out.println(intNum);
-        System.out.println(longNum);
+        byteMax--;
+        shortMax--;
+        intMax--;
+        longMax--;
+        System.out.println("\n Значение после декремента \n" + byteMax + "\n" + shortMax + "\n" + intMax + "\n" + longMax);
 
         System.out.println("\n 5.Перестановка значений переменных");
         int x = 2;
         int y = 5;
+
         System.out.println("x = " + x + ", y = " + y + ".");
         System.out.println("I. C помощью третьей переменной");
         int temp = x;
@@ -91,38 +80,46 @@ public class VariablesTheme {
         y ^= x;
         x ^= y;
         System.out.println("x = " + x + ", y = " + y + ".");
+
         System.out.println("\n 6.Вывод символов и их кодов");
-        char char35 = '#';
-        char char38 = '&';
-        char char64 = '@';
-        char char94 = '^';
-        char char95 = '_';
+        char char35 = 35;
+        char char38 = 38;
+        char char64 = 64;
+        char char94 = 94;
+        char char95 = 95;
+
         System.out.println("35 = " + char35);
         System.out.println("38 = " + char38);
         System.out.println("64 = " + char64);
         System.out.println("94 = " + char94);
         System.out.println("95 = " + char95);
+
         System.out.println("\n 7.Вывод в консоль ASCII-арт Дюка");
         char char92 = 92;
-        System.out.println("    /" + char92 + "     ");
-        System.out.println("   /  " + char92 + "    ");
-        System.out.println("  /_( )" + char92 + "    ");
-        System.out.println(" /      " + char92 + "   ");
-        System.out.println("/____/" + char92 + "__" + char92 );
+        String str01 = ("    /" + char92);
+        String str02 = ("\n   /  " + char92);
+        String str03 = ("\n  /_( )" + char92);
+        String str04 = ("\n /      " + char92);
+        String str05 = ("\n/____/" + char92 + "__" + char92);
+        System.out.println(str01 + str02 + str03 + str04 + str05);
+
         System.out.println("\n 8.Вывод количества сотен, десятков и единиц числа");
-        int n = 123;
-        int m = n/100;
-        int v = n/10;
-        int z = n - v*10;
-        int sum = m + v + z;
-        int prod = m * v * z;
-        System.out.println("Число " + n + " содержит: " + m + " сотню " + v + " десятков " + z + " единиц");
+        int num = 123;
+        int hundred = num / 100;
+        int tens = num / 10;
+        int units = num % 10;
+        int sum = hundred + tens + units;
+        int prod = hundred * tens * units;
+        System.out.println("Число " + num + " содержит: " + hundred + " сотню " + tens + " десятков " + units + " единицы");
         System.out.println("Сумма его цифр = " + sum + ", Произведение = " + prod);
+     
+
+
         System.out.println("\n 9.Вывод времени");
         int time = 86399;
-        int hours = time/60/60;
-        int min = time/60%100;
-        int sec = time%863;
+        int hours = time / 60 / 60;
+        int min = time / 60 % 100;
+        int sec = time % 100;
 
         System.out.println(hours + ":" + min + ":" + sec);
     }
